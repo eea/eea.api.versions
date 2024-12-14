40caf9f7a0884ddef23aae5ff04edb64ef693dc6
David Ichim <ichim.david@gmail.com>
2024-12-14 21:47:57 +0200

    added forward and backward relationship info from eea.relations package
    - With some simplifications for getting only the title, id and type of object
      since we only need to have a listing of versions within a block

--- eea/api/versions/restapi/get.py
+++ eea/api/versions/restapi/get.py
@@ -3,11 +3,11 @@ This module provides the GET endpoint for retrieving version information
 for Plone content items.
 """
 
+from zope.component import queryMultiAdapter
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
 
 from zope.interface import Interface
-
 from zope.interface import implementer
 from zope.component import adapter
 
@@ -27,12 +27,19 @@ class EEAVersions:
                 "@id": f"{self.context.absolute_url()}/@eea.versions"
             }
         }
+
+        relations = queryMultiAdapter(
+            (self.context, self.request), name="eea_versions"
+        )
+
         res = {
             "newer_versions": {
-                "@id": f"{self.context.absolute_url()}/@newer-versions"
+                "@id": f"{self.context.absolute_url()}/@newer-versions",
+                "items": relations.newer_versions() if relations else []
             },
             "older_versions": {
-                "@id": f"{self.context.absolute_url()}/@older-versions"
+                "@id": f"{self.context.absolute_url()}/@older-versions",
+                "items": relations.older_versions() if relations else []
             }
         }
         result["eea.versions"].update(res)
