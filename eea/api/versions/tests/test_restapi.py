45831db564e08546ad0612436dfd3c2c06dfdac8
eea-jenkins <eea-github@googlegroups.com>
2026-04-14 11:30:29 +0200

    style: Automated code fix

--- eea/api/versions/tests/test_restapi.py
+++ eea/api/versions/tests/test_restapi.py
@@ -23,6 +23,7 @@ class TestEEAVersionsSetup(unittest.TestCase):
     def test_product_installed(self):
         """Test that eea.api.versions is installed"""
         from Products.CMFPlone.utils import get_installer
+
         installer = get_installer(self.portal, self.layer["request"])
         self.assertTrue(installer.is_product_installed("eea.api.versions"))
 
@@ -37,6 +38,7 @@ class TestEEAVersionsSetup(unittest.TestCase):
     def test_manager_role(self):
         """Test that test user has Manager role"""
         from plone.app.testing import TEST_USER_ID
+
         roles = self.portal.acl_users.getUserById(TEST_USER_ID).getRoles()
         self.assertIn("Manager", roles)
 
@@ -54,27 +56,34 @@ class TestEEAVersionsView(unittest.TestCase):
         """Test that EEAVersions adapter is registered"""
         from zope.component import queryMultiAdapter
         from zope.interface import Interface
-        adapter = queryMultiAdapter((self.portal, self.portal.REQUEST), Interface, name="eea.versions")
+
+        adapter = queryMultiAdapter(
+            (self.portal, self.portal.REQUEST), Interface, name="eea.versions"
+        )
         # Adapter may or may not be registered depending on config
         # Just check the import works
         from eea.api.versions.restapi.get import EEAVersions
+
         self.assertIsNotNone(EEAVersions)
 
     def test_eea_versions_get_class_exists(self):
         """Test that EEAVersionsGet class exists"""
         from eea.api.versions.restapi.get import EEAVersionsGet
+
         self.assertIsNotNone(EEAVersionsGet)
 
     def test_eea_versions_returns_dict(self):
         """Test that EEAVersions returns a dict"""
         from eea.api.versions.restapi.get import EEAVersions
         from zope.interface import Interface
+
         result = EEAVersions(self.portal, self.portal.REQUEST)()
         self.assertIsInstance(result, dict)
 
     def test_eea_versions_has_id(self):
         """Test that EEAVersions response has eea.versions key"""
         from eea.api.versions.restapi.get import EEAVersions
+
         result = EEAVersions(self.portal, self.portal.REQUEST)()
         self.assertIn("eea.versions", result)
 
